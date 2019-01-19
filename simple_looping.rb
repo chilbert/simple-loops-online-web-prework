4a5f9388c6c1267ac28285ccfd70b97debd8eebd
Chase Hilbert <chase@heyspacetime.com>
2019-01-19 20:13:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -52,7 +52,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0..number_of_times
-  for number in counter
+  for number in counter do
     puts phrase
   end
 end
